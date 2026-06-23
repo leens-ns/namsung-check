e49c1731e0aa116c669dcf36f481201458fceb66
leens-ns <leens@nsworld.net>
2026-06-24 07:06:58 +0900

    Refresh app cache for retention controls

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const APP_URL = "./";
-const CACHE_NAME = "namsung-attendance-20260623-5";
+const CACHE_NAME = "namsung-attendance-20260623-6";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260623-4",
-  "./app.js?v=20260623-4",
+  "./styles.css?v=20260623-6",
+  "./app.js?v=20260623-6",
   "./config.js",
   "./manifest.webmanifest",
-  "./manual.html?v=20260623-5",
-  "./manual.js?v=20260623-5",
+  "./manual.html?v=20260623-6",
+  "./manual.js?v=20260623-6",
   "./logo.svg",
   "./icon-192.png",
   "./icon-512.png",
