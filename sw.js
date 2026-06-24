436593b916326d754e2e88204f9431ae51d5ffb9
leens-ns <leens@nsworld.net>
2026-06-24 09:34:08 +0900

    Refresh cache for lookup improvements

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const APP_URL = "./";
-const CACHE_NAME = "namsung-attendance-20260624-1";
+const CACHE_NAME = "namsung-attendance-20260624-2";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260624-1",
-  "./app.js?v=20260624-1",
+  "./styles.css?v=20260624-2",
+  "./app.js?v=20260624-2",
   "./config.js",
   "./manifest.webmanifest",
-  "./manual.html?v=20260624-1",
-  "./manual.js?v=20260624-1",
+  "./manual.html?v=20260624-2",
+  "./manual.js?v=20260624-2",
   "./logo.svg",
   "./icon-192.png",
   "./icon-512.png",
