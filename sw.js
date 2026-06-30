31220493bbccc3f522099c1dbc6ea0a67743282c
leens-ns <leens@nsworld.net>
2026-06-30 21:18:52 +0900

    Update sw.js for department and notification UX

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const APP_URL = "./";
-const CACHE_NAME = "namsung-attendance-20260625-3";
+const CACHE_NAME = "namsung-attendance-20260630-1";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260625-3",
-  "./app.js?v=20260625-3",
+  "./styles.css?v=20260630-1",
+  "./app.js?v=20260630-1",
   "./config.js",
   "./manifest.webmanifest",
-  "./manual.html?v=20260625-3",
-  "./manual.js?v=20260625-3",
+  "./manual.html?v=20260630-1",
+  "./manual.js?v=20260630-1",
   "./logo.svg",
   "./icon-192.png",
   "./icon-512.png",
